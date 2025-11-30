7434e6523842b63ce10554bc9eed6360aa012905
pravinjoshu <pravinvip003@gmail.com>
2025-11-30 22:10:51 +0530

    addproducts working

--- src/admin/components/AdminLayout/AdminLayout.jsx
+++ src/admin/components/AdminLayout/AdminLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar.jsx';
 import Navbar from '../Navbar/Navbar.jsx';
@@ -7,12 +7,39 @@ import './AdminLayout.css';
 
 const AdminLayout = () => {
     const { sidebarCollapsed } = useApp();
+    const [sideWidth,setSideWidth] = useState(true)
+    console.log(sideWidth)
+
+    useEffect(() => {
+    const handleResize = () => {
+        const width = window.innerWidth;
+
+        if (width <= 768) {
+            // Mobile
+            setSideWidth(false);
+        } 
+        else if (width > 768 && width <= 1024) {
+            // Tablet — your required behavior
+            setSideWidth(true);       // auto shrink sidebar
+        } 
+        else {
+            // Desktop like 1440px
+            setSideWidth(true);       // keep normal sidebar open
+        }
+    };
+
+    handleResize(); // run on first load
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+}, []);
+
 
     return (
         <div className="admin-layout">
-            <Sidebar />
+            <Sidebar setSideWidth={setSideWidth} sideWidth={sideWidth} />
             <div className={`admin-main ${sidebarCollapsed ? 'sidebar-collapsed' : ''}`}>
-                <Navbar />
+                <Navbar sideWidth={sideWidth} />
                 <div className="admin-content">
                     <Outlet />
                 </div>
