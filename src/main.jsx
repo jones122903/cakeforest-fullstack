d8d9a03d29e3f5ef512d44e1079283ecb6a60dd4
pravinjoshu <pravinvip003@gmail.com>
2025-12-10 10:37:31 +0530

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './config.css'
-import App from './app/App.jsx'
-import { registerSW } from 'virtual:pwa-register'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./config.css";
+import App from "./app/App.jsx";
+import { registerSW } from "virtual:pwa-register";
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
 
 // Register service worker with auto-update
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (confirm('New content available. Reload to update?')) {
-      updateSW(true)
+    if (confirm("New content available. Reload to update?")) {
+      updateSW(true);
     }
   },
   onOfflineReady() {
-    console.log('App ready to work offline')
+    console.log("App ready to work offline");
   },
-})
+});
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
+);
